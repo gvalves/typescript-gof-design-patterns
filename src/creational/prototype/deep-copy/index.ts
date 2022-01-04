7326803fd98e389f1a321686343ee5f87ec83c66
gvalves <gva@geekcode.com.br>
2022-01-04 20:55:00 -0300

    Prototype

---
+++ src/creational/prototype/deep-copy/index.ts
@@ -0,0 +1,43 @@
+export interface Prototype {
+  clone(): Prototype;
+}
+
+export class Person implements Prototype {
+  public addresses: Address[] = [];
+
+  constructor(public name: string, public age: number) {}
+
+  clone(): Person {
+    const newObj = new Person(this.name, this.age);
+    newObj.addresses = this.addresses.map((address) => address.clone());
+    return newObj;
+  }
+
+  addAddress(address: Address) {
+    this.addresses.push(address);
+  }
+}
+
+export class Address implements Prototype {
+  constructor(public street: string, public number: number) {}
+
+  clone(): Address {
+    return new Address(this.street, this.number);
+  }
+}
+
+const address1 = new Address('Av Brasil', 15);
+
+const person1 = new Person('Luiz', 30);
+person1.addAddress(address1);
+
+const person2 = person1.clone();
+person2.name = 'Joana';
+
+person1.addresses[0].street = 'Bla Bla Bla';
+
+console.log(person1);
+console.log(person2.addresses);
+
+console.log(person2);
+console.log(person2.addresses);
